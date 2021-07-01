465153b79f5a267001c9c53ef0bd2316d8d7880c
Victor Mikell <78280727+vmikell@users.noreply.github.com>
2021-06-30 23:58:11 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 var mobileWindow
 function openMobileWindow() {
   mobileWindow = window.open(
-    'https://unruffled-beaver-253aed.netlify.app',
+    'https://victormikell-test.netlify.app',
     '_blank',
     'toolbar=0,location=0,menubar=0,scrollbars=0,height=677,width=400,left=100,top=50'
   )
 }
 
-
 let jsImg = document.getElementById('js-img-btn')
-jsImg.addEventListener('click', function (){
+jsImg.addEventListener('click', function () {
   rotateImg()
 })
 
-let rotation = Math.random() * (360 - 0) + 0;
+let rotation = Math.random() * (360 - 0) + 0
 function rotateImg() {
   rotation += 90
-  if(rotation === 360){
+  if (rotation === 360) {
     rotation = 0
   }
-  document.getElementById('js-img').style.transform = 'rotate(' + rotation + 'deg)'
+  document.getElementById('js-img').style.transform =
+    'rotate(' + rotation + 'deg)'
 }
