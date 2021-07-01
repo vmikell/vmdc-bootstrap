cde3b1fe3f6de9cc35a80ef4f31b75519bf67edf
Victor Mikell <78280727+vmikell@users.noreply.github.com>
2021-06-30 23:54:47 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const lightbox = GLightbox({
-  href: 'https://www.youtube.com/watch?v=Ga6RYejo6Hk',
-  type: 'video',
-  source: 'youtube', //vimeo, youtube or local
-  width: 900,
-})
-
 var mobileWindow
 function openMobileWindow() {
   mobileWindow = window.open(
@@ -12,4 +5,19 @@ function openMobileWindow() {
     '_blank',
     'toolbar=0,location=0,menubar=0,scrollbars=0,height=677,width=400,left=100,top=50'
   )
+}
+
+
+let jsImg = document.getElementById('js-img-btn')
+jsImg.addEventListener('click', function (){
+  rotateImg()
+})
+
+let rotation = Math.random() * (360 - 0) + 0;
+function rotateImg() {
+  rotation += 90
+  if(rotation === 360){
+    rotation = 0
+  }
+  document.getElementById('js-img').style.transform = 'rotate(' + rotation + 'deg)'
 }
